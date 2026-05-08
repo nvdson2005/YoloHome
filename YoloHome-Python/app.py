17bb818918ebcd2c53a375b7026b882b207eea82
Son Nguyen <ducsonds2005@gmail.com>
2026-05-08 23:20:35 +0700

    choore: enable CORS

--- YoloHome-Python/app.py
+++ YoloHome-Python/app.py
@@ -11,9 +11,6 @@ data = []
 
 app = FastAPI()
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
     "http://localhost:8080",
     "http://localhost:5173",
 ]
